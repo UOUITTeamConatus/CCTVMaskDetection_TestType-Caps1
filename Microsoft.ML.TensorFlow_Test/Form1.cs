8b1f26a0cd703a13eaecd81d2895d39f9cc7f598
swj1265 <swj1265@naver.com>
2020-11-24 16:21:14 +0900

    What the...

--- Microsoft.ML.TensorFlow_Test/Form1.cs
+++ Microsoft.ML.TensorFlow_Test/Form1.cs
@@ -10,16 +10,33 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.ML;
 using Microsoft.ML.Data;
-using.Microsoft.ML.Transforms;
-
+using Microsoft.ML.Transforms;
+using OpenCvSharp;
+using OpenCvSharp.Dnn;
+using OpenCvSharp.Extensions;
+using Keras;
 
 namespace Microsoft.ML.TensorFlow_Test
 {
     public partial class Form1 : Form
     {
+        //DirectoryInfo mediaDIR = new DirectoryInfo(Program.mediaPath);
+        private string mediaPath = System.Windows.Forms.Application.StartupPath.ToString + Program.mediaPath;
+        private string prototxtPath = System.Windows.Forms.Application.StartupPath + Program.prototxtPath;
+        private string caffemodelPath = System.Windows.Forms.Application.StartupPath + Program.caffemodelPath;
+        private string maskdetectorPath = System.Windows.Forms.Application.StartupPath + Program.maskdetectorPath;
+
+
         public Form1()
         {
+            //var net = CvDnn.ReadNetFromCaffe(Program.prototxt, Program.caffemodel);
+            //var model = Keras.Models.Model.LoadModel(Program.Maskmodel);
             InitializeComponent();
+            Console.WriteLine(Program.mediaPath);
+            Console.WriteLine(Program.prototxtPath);
+            Console.WriteLine(Program.caffemodelPath);
+            Console.WriteLine(Program.maskdetectorPath);
+            Console.WriteLine(System.Windows.Forms.Application.StartupPath);
         }
 
         private void pictureBoxIpl1_Click(object sender, EventArgs e)
--- Microsoft.ML.TensorFlow_Test/Program.cs
+++ Microsoft.ML.TensorFlow_Test/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -8,12 +10,25 @@ namespace Microsoft.ML.TensorFlow_Test
 {
     static class Program
     {
+        //public const string startPath = ;
+        public static string currentPath;
+        public const string mediaPath = "models\\media";
+        public const string prototxtPath = "models\\deploy.prototxt";
+        public const string caffemodelPath = "models\\res10_300x300_ssd_iter_140000.caffemodel";
+        public const string maskdetectorPath = "models\\mask_detector.model";
+        /*
+        public static string mediaPath = Path.GetFullPath(path1);
+        public static string prototxt = Path.GetFullPath(path2);
+        public static string caffemodel = Path.GetFullPath(path3);
+        public static string Maskmodel = Path.GetFullPath(path4);*/
         /// <summary>
         /// 해당 애플리케이션의 주 진입점입니다.
         /// </summary>
         [STAThread]
+
         static void Main()
         {
+            currentPath = GetCurrentDirectory();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
