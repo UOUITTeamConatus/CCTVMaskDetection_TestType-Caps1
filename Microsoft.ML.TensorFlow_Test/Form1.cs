109a4f64cac576db24c5068f6fa50fd659267feb
swj1265 <swj1265@naver.com>
2020-11-24 17:39:24 +0900

    초기 세팅

--- Microsoft.ML.TensorFlow_Test/Form1.cs
+++ Microsoft.ML.TensorFlow_Test/Form1.cs
@@ -15,27 +15,29 @@ using OpenCvSharp;
 using OpenCvSharp.Dnn;
 using OpenCvSharp.Extensions;
 using Keras;
+using Python;
+using Python.Runtime;
 
 namespace Microsoft.ML.TensorFlow_Test
 {
     public partial class Form1 : Form
     {
         //DirectoryInfo mediaDIR = new DirectoryInfo(Program.mediaPath);
-        private string mediaPath = System.Windows.Forms.Application.StartupPath.ToString + Program.mediaPath;
-        private string prototxtPath = System.Windows.Forms.Application.StartupPath + Program.prototxtPath;
-        private string caffemodelPath = System.Windows.Forms.Application.StartupPath + Program.caffemodelPath;
-        private string maskdetectorPath = System.Windows.Forms.Application.StartupPath + Program.maskdetectorPath;
+        private string mediaPath = Program.currentPath + "\\" + Program.mediaPath;
+        private string prototxtPath = Program.currentPath + "\\" + Program.prototxtPath;
+        private string caffemodelPath = Program.currentPath + "\\" + Program.caffemodelPath;
+        private string maskdetectorPath = Program.currentPath + "\\" + Program.maskdetectorPath;
 
 
         public Form1()
         {
-            //var net = CvDnn.ReadNetFromCaffe(Program.prototxt, Program.caffemodel);
-            //var model = Keras.Models.Model.LoadModel(Program.Maskmodel);
+            var net = CvDnn.ReadNetFromCaffe(prototxtPath, caffemodelPath);
+            var model = Keras.Models.Model.LoadModel(maskdetectorPath);
             InitializeComponent();
-            Console.WriteLine(Program.mediaPath);
-            Console.WriteLine(Program.prototxtPath);
-            Console.WriteLine(Program.caffemodelPath);
-            Console.WriteLine(Program.maskdetectorPath);
+            Console.WriteLine(mediaPath);
+            Console.WriteLine(prototxtPath);
+            Console.WriteLine(caffemodelPath);
+            Console.WriteLine(maskdetectorPath);
             Console.WriteLine(System.Windows.Forms.Application.StartupPath);
         }
 
--- Microsoft.ML.TensorFlow_Test/Program.cs
+++ Microsoft.ML.TensorFlow_Test/Program.cs
@@ -5,13 +5,15 @@ using System.Text;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Python;
+using Python.Runtime;
 
 namespace Microsoft.ML.TensorFlow_Test
 {
     static class Program
     {
         //public const string startPath = ;
-        public static string currentPath;
+        public static string currentPath = System.Environment.CurrentDirectory;
         public const string mediaPath = "models\\media";
         public const string prototxtPath = "models\\deploy.prototxt";
         public const string caffemodelPath = "models\\res10_300x300_ssd_iter_140000.caffemodel";
@@ -28,10 +30,19 @@ namespace Microsoft.ML.TensorFlow_Test
 
         static void Main()
         {
-            currentPath = GetCurrentDirectory();
+            //currentPath = GetCurrentDirectory();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+        public static void AddEnvPath(params string[] paths)
+        {
+            // PC에 설정되어 있는 환경 변수를 가져온다.
+            var envPaths = Environment.GetEnvironmentVariable("PATH").Split(Path.PathSeparator).ToList();
+            // 중복 환경 변수가 없으면 list에 넣는다.
+            envPaths.InsertRange(0, paths.Where(x => x.Length > 0 && !envPaths.Contains(x)).ToArray());
+            // 환경 변수를 다시 설정한다.
+            Environment.SetEnvironmentVariable("PATH", string.Join(Path.PathSeparator.ToString(), envPaths), EnvironmentVariableTarget.Process);
         }
     }
 }
